aa538304e1e05d0b5d8aa3fafef3b5ccd50cfea7
Amanpatel6232 <amanpatel.ap79099@gmail.com>
2024-02-09 02:09:07 -0800

    name

--- src/Login.js
+++ src/Login.js
@@ -5,9 +5,9 @@ function Login()
         <div className="login1">
             <div className="login2">
                 <form>
-                    <input type="text" placeholder="Enter Name"></input><br></br>
+                    Name<input type="text" placeholder="Enter Name"></input><br></br>
                     <br></br>
-                    <input type="password" placeholder="Enter Passwrod"></input><br></br>
+                    Password<input type="password" placeholder="Enter Passwrod"></input><br></br>
                     <br></br>
                     <button>Submit</button>
                 </form>
